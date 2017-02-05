313c7ae72e7f6912a39888faeb97e7b1da4cbc6b
Oliver Campion <ollie24-10-91@hotmail.com>
2017-02-05 17:52:34 +0000

    Dealers second card is hidden at start of hand. Handling for dealer hitting or sticking during blackjack round.

--- BlackJack/src/com/blackjack/Dealer.java
+++ BlackJack/src/com/blackjack/Dealer.java
@@ -1,13 +1,54 @@
 package com.blackjack;
 
-public class Dealer {
-	private Hand hand;
-	
-	public void setHand(Hand hand){
-		this.hand = hand;
+public class Dealer extends Participant {
+	private boolean secondCardHidden = true;
+
+	public boolean isSecondCardHidden() {
+		return secondCardHidden;
 	}
 
-	public Hand getHand() {
-		return hand;
+	public void setSecondCardHidden(boolean secondCardHidden) {
+		this.secondCardHidden = secondCardHidden;
 	}
+
+	@Override
+	public void hitOrStick(Deck deck) {
+		System.out.println("The dealers cards cards add up to " + getHand().getTotalHandValue());
+		if(getHand().getTotalHandValue()<16){
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		while(true){
+			if(getHand().getTotalHandValue() >= 16 && getHand().getTotalHandValue() <= 21){
+				System.out.println("The dealer has stuck with this hand");
+				break;
+			}
+			else if(getHand().getTotalHandValue() < 16){
+				System.out.println("The dealer hits");
+				getHand().addCardToHand(deck);
+				System.out.println();
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				getHand().outputCurrentHand(this);
+				getHand().outputHandValue(this);
+				System.out.println();
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if(getHand().isHandBust()){
+					break;
+				}
+			}
+		}
+	}
+	
+	
 }

9e034fe2a92c983b5593c84485a0589321924454
Oliver Campion <ollie24-10-91@hotmail.com>
2017-02-05 18:04:04 +0000

    Updated the Ace Value.

--- BlackJack/src/com/blackjack/Rank.java
+++ BlackJack/src/com/blackjack/Rank.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public enum Rank {
 	TWO(2, 2), THREE(3, 3), FOUR(4, 4), FIVE(5, 5), SIX(6, 6), SEVEN(7, 7), EIGHT(8, 8), 
-    NINE(9, 9), TEN(10, 10), JACK(11, 10), QUEEN(12, 10), KING(13, 10), ACE(14, 0);
+    NINE(9, 9), TEN(10, 10), JACK(11, 10), QUEEN(12, 10), KING(13, 10), ACE(14, 1);
 	
 	private int id;
 	private int value;
@@ -13,23 +13,6 @@ public enum Rank {
 	private Rank(int id, int value){
 		this.id = id;
 		this.value = value;
-	}
-	
-	private int getAceValue(){
-		System.out.println("Please choose 1 or 11 for the value of your Ace");
-		System.out.println("Enter Ace Value: ");
-		int aceValue;
-		while(true){
-			aceValue = sc.nextInt();
-			if(aceValue!=1 || aceValue!=11){
-				System.out.println("Error please choose 1 or 11 for your Ace Value: ");
-			}
-			else{
-				break;
-			}
-		}
-		
-		return aceValue;
 	}
 
 	public int getId() {

30252471fe46b9b2924139cbdfe1492e2d5eb624
Oliver Campion <ollie24-10-91@hotmail.com>
2017-02-05 17:47:16 +0000

    Fixed issue where invalid bets were allowed.

--- BlackJack/src/com/blackjack/Bet.java
+++ BlackJack/src/com/blackjack/Bet.java
@@ -5,17 +5,24 @@ import java.util.Scanner;
 
 public class Bet {
 	private static Scanner sc = new Scanner(System.in);
-	private BigDecimal bet;
+	private BigDecimal amountBet;
 	
 	public void makeFirstBet(){
 		while(true){
 			System.out.println("Please enter the amount you wish to bet.");
 			System.out.println("The minimum bet is 2, the maximum bet is 100.");
 			System.out.println("The bet must be in 50c intervals, e.g €5.00, €5.50, €6.00");
-			System.out.println("Enter your bet: ");
-			bet = sc.nextBigDecimal();
-			if(isBetValid(bet)){
-				System.out.println("Your bet of " + CashHandler.formatCurrencyForDisplay(bet) + " is valid, please wait hands will be dealt.");
+			System.out.print("Enter your bet: ");
+			amountBet = sc.nextBigDecimal();
+			if(isBetValid(amountBet)){
+				System.out.println();
+				System.out.println("Your bet of " + CashHandler.formatCurrencyForDisplay(amountBet) + " is valid, please wait hands will be dealt.");
+				System.out.println();
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 				break;
 			}
 		}
@@ -23,15 +30,21 @@ public class Bet {
 
 	public boolean isBetValid(BigDecimal bet){
 		if(bet.doubleValue()<2){
+			System.out.println();
 			System.out.println("The bet is too small, please bet again.");
+			System.out.println();
 			return false;
 		}
 		else if(bet.doubleValue() > 100){
+			System.out.println();
 			System.out.println("The bet is too large, please bet again.");
+			System.out.println();
 			return false;
 		}
-		else if((bet.doubleValue() * 100) % 5 != 0){
+		else if((bet.doubleValue() * 100) % 50 != 0){
+			System.out.println();
 			System.out.println("The bet must be in 50c intervals, e.g €5.00, €5.50, €6.00, please bet again");
+			System.out.println();
 			return false;
 		}
 		else{
@@ -39,11 +52,11 @@ public class Bet {
 		}
 	}
 	
-	public BigDecimal getBet() {
-		return bet;
+	public BigDecimal getAmountBet() {
+		return amountBet;
 	}
 
-	public void setBet(BigDecimal bet) {
-		this.bet = bet;
+	public void setAmountBet(BigDecimal amountBet) {
+		this.amountBet = amountBet;
 	}
 }

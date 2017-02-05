a20e9d6d4fc6c8d1597837c6122907bdc2dffba2
Oliver Campion <ollie24-10-91@hotmail.com>
2017-02-05 17:58:50 +0000

    Multiple blackjack rounds can now be played.

--- BlackJack/src/com/blackjack/Driver.java
+++ BlackJack/src/com/blackjack/Driver.java
@@ -1,11 +1,16 @@
 package com.blackjack;
 
+import java.util.Scanner;
+
 public class Driver {
+	private static Scanner sc = new Scanner(System.in);
 	public static void main(String [] args){
 		BlackJack blackJack = new BlackJack();
 		blackJack.joinBlackJackTable();
-		blackJack.handleBetting();
-		blackJack.dealStartingHands();
-		blackJack.playRoundOfBlackJack();
+		while(blackJack.playerWantsToPlay()){
+			blackJack.handleBetting();
+			blackJack.dealStartingHands();
+			blackJack.playRoundOfBlackJack();
+		}
 	}
 }

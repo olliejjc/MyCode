797a58178bc8111410145bc09c5cc30d0268db1a
eolcamp <eolcamp@IE00200825.ericsson.se>
2017-02-06 16:18:13 +0000

    Player can no longer make a bet that exceeds current amount of cash.

--- BlackJack/src/com/blackjack/Bet.java
+++ BlackJack/src/com/blackjack/Bet.java
@@ -7,14 +7,15 @@ public class Bet {
 	private static Scanner sc = new Scanner(System.in);
 	private BigDecimal amountBet;
 	
-	public void makeFirstBet(){
+	public void makeBet(Player player){
+		BigDecimal cash = player.getCash();
 		while(true){
 			System.out.println("Please enter the amount you wish to bet.");
 			System.out.println("The minimum bet is 2, the maximum bet is 100.");
 			System.out.println("The bet must be in 50c intervals, e.g €5.00, €5.50, €6.00");
 			System.out.print("Enter your bet: ");
 			amountBet = sc.nextBigDecimal();
-			if(isBetValid(amountBet)){
+			if(isBetValid(amountBet, cash)){
 				System.out.println();
 				System.out.println("Your bet of " + CashHandler.formatCurrencyForDisplay(amountBet) + " is valid, please wait hands will be dealt.");
 				System.out.println();
@@ -28,14 +29,14 @@ public class Bet {
 		}
 	}
 
-	public boolean isBetValid(BigDecimal bet){
+	public boolean isBetValid(BigDecimal bet, BigDecimal cash){
 		if(bet.doubleValue()<2){
 			System.out.println();
 			System.out.println("The bet is too small, please bet again.");
 			System.out.println();
 			return false;
 		}
-		else if(bet.doubleValue() > 100){
+		else if(bet.doubleValue() > 100 || bet.compareTo(cash) == 1){
 			System.out.println();
 			System.out.println("The bet is too large, please bet again.");
 			System.out.println();
